ef775e24eb71d889a1638afd364b0e281e9b0e5c
Gancho <gancho@paiyakdev.com>
2020-03-11 18:28:01 +0200

    Background photos pre-loader and firefox support

--- web/app/themes/wp-photo-theme/functions.php
+++ web/app/themes/wp-photo-theme/functions.php
@@ -19,24 +19,40 @@ function wpt_register_scripts() {
     $theme_version = wp_get_theme()->get( 'Version' );
     wp_enqueue_script( 'main-script', get_template_directory_uri() . '/js/main-script.js', ['jquery'], $theme_version, true );
 
-    $background_photos = [];
-	$frontpage_id = get_option( 'page_on_front' );
-	for ( $i = 1; $i <= 3; $i++ ) {
-		$field = get_field( "background_photo_$i", $frontpage_id ) ;
-		if ( $field ) {
-			$background_photos[] = $field;
-		}
-	}
-	$translation_array = array(
-	    'some_string' => __( 'Some string to translate', 'plugin-domain' ),
-	    'background_photos' => $background_photos
-	);
-	wp_localize_script( 'main-script', 'site_data', $translation_array );
+    $wpt_js_data = [
+        'some_string' => __( 'Some string to translate', 'plugin-domain' ),
+    ];
+
+    $wpt_js_data = wpt_inject_background_photos( $wpt_js_data );
+	wp_localize_script( 'main-script', 'wpt_js_data', $wpt_js_data );
 
 }
 add_action( 'wp_enqueue_scripts', 'wpt_register_scripts' );
 
+/**
+ * Get background photos fields from the home page (ACF based) and add them to the javascript data, if there is any
+ * and the ACF plugin is installed
+ *
+ * @param array $wpt_js_data Javascript array data which will be injected
+ *
+ * @return array $wpt_js_data Javascript array with injected data
+ */
 
+function wpt_inject_background_photos( $wpt_js_data ) {
+    if ( ! function_exists("get_field" ) ) return $wpt_js_data;
+    $background_photos = [];
+   	$frontpage_id = get_option( 'page_on_front' );
 
-// Enqueued script with localized data.
-// wp_enqueue_script( 'some_handle' );
+   	$i = 1;
+    $field = get_field( "background_photo_$i", $frontpage_id );
+   	while( ! is_null( $field ) ) {
+   	    if ( $field ) $background_photos[] = $field;
+   	    $i++;
+        $field = get_field( "background_photo_$i", $frontpage_id ) ;
+    }
+    $wpt_js_data[ 'background_photos' ] = $background_photos;
+
+    $background_photos_interval = get_field( 'background_photos_interval', $frontpage_id );
+    $wpt_js_data[ 'background_photos_interval' ] = $background_photos_interval;
+    return ( $wpt_js_data );
+}
--- web/app/themes/wp-photo-theme/js/main-script.js
+++ web/app/themes/wp-photo-theme/js/main-script.js
@@ -13,20 +13,64 @@ jQuery( '.mobile-menu-button' ).on( 'click', function() {
 	jQuery('.mobile-menu-wrapper').toggleClass( 'm-active' )
 } );
 
-
-setInterval( function() {
-	let $banner = jQuery( '.wpt-page-banner' );
-	let num = $banner.attr( 'data-num' );
-	num = undefined === num ? 0 : num;
-	if ( num >= site_data.background_photos.length )
-		num = 0;
-
-	if ( site_data.background_photos[num] && site_data.background_photos[num].sizes ) {
-		$banner.css( 'background-image', 'url("' + site_data.background_photos[num].sizes.large + '")' );
-		num++;
-		$banner.attr( 'data-num', num );
+function wpt_preload_background_photos () {
+	let loaded_images = new Array();
+	if ( wpt_js_data.background_photos ) {
+		for( let i = 0; i < wpt_js_data.background_photos.length; i++ ) {
+			let img_url = wpt_js_data.background_photos[i].sizes.large;
+			if ( ! img_url ) continue;
+			var img = new Image();
+			img.onload = function() {
+				var index = loaded_images.indexOf( this );
+				if ( index !== -1 ) {
+					// remove image from the array once it's loaded
+					// for memory consumption reasons
+					loaded_images.splice( index, 1 );
+				}
+			}
+			loaded_images.push( img );
+			img.src = img_url;
+		}
 	}
-	
-}, 5000 );
+}
 
-// console.log( site_data );
+function wpt_rotate_photos() {
+	wpt_preload_background_photos();
+	jQuery( '.wpt-page-banner' ).css( {'background-color': 'transparent', 'background-image' : 'none'} );
+	if ( ! wpt_js_data.background_photos &&  ! wpt_js_data.background_photos.length ) return;
+	wpt_set_background_photo();
+	let background_photos_interval = wpt_js_data.background_photos_interval ? wpt_js_data.background_photos_interval : 5000;
+		setInterval( wpt_set_background_photo, background_photos_interval * 1 + 4000 );
+}
+wpt_rotate_photos();
+
+function wpt_set_background_photo( ) {
+	//Because firefox do not support background-image transitions, we have to do it the hard way
+	//We create two containers. The top container fades in then is removed.
+	let $banner_container = jQuery('.wpt-page-banner');
+	let num = $banner_container.attr( 'data-num' );
+	if ( undefined === num ) {
+		$banner_container.attr( 'data-num', 0 );
+		let bgr_url =  wpt_js_data.background_photos[0].sizes.large;
+		$banner_container.prepend('<div id="banner" style="position: absolute; top: 0; bottom: 0; left: 0; right: 0; background-image: url('+bgr_url+'); z-index: -1"></div>');
+		return;
+	}
+	num1 = num * 1 + 1;
+	num1 = wpt_js_data.background_photos[num1] ? num1 : 0;
+	if ( ! wpt_js_data.background_photos[num1].sizes  || ! wpt_js_data.background_photos[num1].sizes.large ) {
+		$banner_container.attr( 'data-num', num1 );
+		return;
+	}
+	let new_bgr_url = wpt_js_data.background_photos[num1].sizes.large;
+
+	let $banner = jQuery('#banner');
+// debugger;
+	$banner.after( '<div id="banner1" style="position: absolute; top: 0; bottom: 0; left: 0; right: 0; background-color: blue; z-index: -1; opacity: 0; background-image: url('+ new_bgr_url +')"></div>' );
+	let $banner1 = jQuery( '#banner1' );
+	$banner1.animate( {opacity: '1'}, "2000", 'swing', function() {
+		$banner.css( 'background-image', 'url(' + new_bgr_url + ')' ).animate( { opacity: '1' }, 100, 'swing', function() {
+			$banner1.remove();
+		} );
+	});
+	$banner_container.attr( 'data-num', num1 );
+}
